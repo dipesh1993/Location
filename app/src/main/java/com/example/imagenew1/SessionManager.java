f07fbcee87ad774753e85676cffcfedfdbe2d19f
dipes <dipesh.shivade1234@gmail.com>
2020-10-01 17:16:53 +0530

    commit

--- app/src/main/java/com/example/imagenew1/SessionManager.java
+++ app/src/main/java/com/example/imagenew1/SessionManager.java
@@ -21,7 +21,7 @@ public class SessionManager {
 
     public SessionManager(Context context) {
         this.context = context;
-        sharedPreferences = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        sharedPreferences = context.getSharedPImageNew1references(PREF_NAME, PRIVATE_MODE);
         editor = sharedPreferences.edit();
     }
 
